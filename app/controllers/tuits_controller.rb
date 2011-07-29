bc80ec21cef21e70aef5a1cf5d2f511aee743fac
Francisco M. Pérez Padilla <francis@trasto.(none)>
2011-07-29 17:40:41 +0200

    show tuits for a single user

--- app/controllers/tuits_controller.rb
+++ app/controllers/tuits_controller.rb
@@ -1,7 +1,9 @@
 class TuitsController < ApplicationController
+  before_filter :login_required, :except => :index
+
   def index
-    @tuits = Tuit.all
-    @tuit  = current_user.tuits.build
+    @tuits = tuits_list
+    @tuit  = current_user.tuits.build if logged_in?
   end
 
   def create
@@ -9,8 +11,19 @@ class TuitsController < ApplicationController
     if @tuit.save
       redirect_to tuits_url, :notice => "Successfully created tuit."
     else
-      @tuits = Tuit.all
+      @tuits = tuits_list
       render :action => 'index'
+    end
+  end
+
+  private
+
+  def tuits_list
+    @user = User.find(params[:user_id]) if params[:user_id]
+    if @user
+      @user.tuits
+    else
+      Tuit.all
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,13 @@
 Tuiter::Application.routes.draw do
   match 'user/edit' => 'users#edit', :as => :edit_current_user
-
   match 'signup' => 'users#new', :as => :signup
-
   match 'logout' => 'sessions#destroy', :as => :logout
-
   match 'login' => 'sessions#new', :as => :login
 
   resources :sessions
-
-  resources :users
-
+  resources :users do
+    resources :tuits, :only => :index
+  end
   resources :tuits, :only => [:index, :create]
 
   root :to => "tuits#index"
